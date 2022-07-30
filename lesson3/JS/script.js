0e24e7390d137ed072e9ceed7ad1f4593921f5c6
mishamuzyka2291 <219misha219@gmail.com>
2022-07-30 22:27:19 +0300

    first task

--- lesson3/JS/script.js
+++ lesson3/JS/script.js
@@ -1,2 +1,14 @@
-let b = 10;
-console.log(b);
+//first task
+
+let x;
+let a = +prompt("Put number for example.");
+x = a;
+ if (a!==0){
+    console.log("Correct");
+}
+else {
+    console.log("Incorrect");
+}
+
+//second task
+
